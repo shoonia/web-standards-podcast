1096698f9899b963add84fa728a0cd50c7c7a968
shoonia <zaytsev126@gmail.com>
2019-05-30 19:03:11 +0300

    Page 404

---
+++ src/layouts/NotFound/index.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'gatsby';
+import Helmet from 'react-helmet';
+
+import Document from '../../components/Document';
+
+const NotFound = () => (
+  <Document
+    title="404"
+  >
+    <Helmet
+      meta={[{
+        name: 'robots',
+        content: 'noindex',
+      }]}
+    />
+    <div>
+      <p>
+        Ошибка 404: страница не найдена
+      </p>
+      <Link
+        to="/"
+      >
+        Вернуться на главную страницу
+      </Link>
+    </div>
+  </Document>
+);
+
+export default NotFound;
--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,9 +1,1 @@
-import React from 'react';
-
-const NotFoundPage = () => (
-  <div>
-    Not Found
-  </div>
-);
-
-export default NotFoundPage;
+export { default } from '../layouts/NotFound';
