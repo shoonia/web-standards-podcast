c3cde1b108918373453fd95f6a40d1894cf80a66
Alexander Zaytsev <zaytsev126@gmail.com>
2019-05-31 01:16:25 +0300

    [FIX] document

--- src/components/Audio/util.js
+++ src/components/Audio/util.js
@@ -1,20 +1,18 @@
 /* eslint-disable no-restricted-globals */
-const doc = document;
-
 export default {
   getVolume() {
-    const cookie = doc.cookie.replace(/(?:(?:^|.*;\s*)volume\s*=\s*([^;]*).*$)|^.*$/, '$1');
+    const cookie = document.cookie.replace(/(?:(?:^|.*;\s*)volume\s*=\s*([^;]*).*$)|^.*$/, '$1');
     const volume = parseFloat(cookie, 10);
 
     return isNaN(volume) ? 0.5 : volume;
   },
 
   setVolume(volume) {
-    doc.cookie = `volume=${volume};path=/;max-age=${60 * 60 * 24 * 365}`;
+    document.cookie = `volume=${volume};path=/;max-age=${60 * 60 * 24 * 365}`;
   },
 
   getCurrentTime() {
-    const val = doc.location.hash.slice(1);
+    const val = document.location.hash.slice(1);
     const time = parseInt(val, 10);
 
     return isNaN(time) ? 0 : time;
