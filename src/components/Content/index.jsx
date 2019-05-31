2855f8462c6788cece15f67f51150ac23b5266d6
Alexander Zaytsev <zaytsev126@gmail.com>
2019-06-01 00:39:41 +0300

    Link blank

--- src/components/Content/index.jsx
+++ src/components/Content/index.jsx
@@ -3,8 +3,19 @@ import React from 'react';
 import xss from 'xss';
 import PropTypes from 'prop-types';
 
+const options = {
+  onTag(tag, html) {
+    if (tag === 'a') {
+      const a = html.slice(0, -1);
+      return `${a} target="_blank" rel="noopener noreferrer">`;
+    }
+
+    return html;
+  },
+};
+
 const Content = ({ html }) => (
-  <div dangerouslySetInnerHTML={{ __html: xss(html) }} />
+  <div dangerouslySetInnerHTML={{ __html: xss(html, options) }} />
 );
 
 Content.propTypes = {
