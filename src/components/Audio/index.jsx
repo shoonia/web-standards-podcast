ae92e64a49f25e059b2466e4d1253da4e1508b2d
Alexander Zaytsev <zaytsev126@gmail.com>
2019-06-09 17:33:04 +0300

    New player

--- src/components/Audio/index.jsx
+++ src/components/Audio/index.jsx
@@ -1,63 +1,99 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import util from './util';
 
-class Audio extends React.PureComponent {
+import util from './util';
+import css from './AudioPlayer.module.css';
+
+class AudioPlayer extends React.PureComponent {
   static propTypes = {
     lang: PropTypes.string.isRequired,
-    label: PropTypes.string.isRequired,
+    // label: PropTypes.string.isRequired,
     url: PropTypes.string.isRequired,
     type: PropTypes.string.isRequired,
   }
 
   constructor(props) {
     super(props);
-    this.audio = React.createRef();
+    const audio = new Audio();
+
+    audio.type = props.type;
+    audio.lang = props.lang;
+    audio.preload = 'auto';
+
+    this.audio = audio;
+    this.progress = React.createRef();
+
+    this.state = {
+      isDisabled: true,
+    };
   }
 
   componentDidMount() {
-    this.audio.current.volume = util.getVolume();
+    const { url } = this.props;
+    const time = util.getCurrentTime();
+    const progress = this.progress.current;
+
+    this.audio.src = url;
+
+    this.audio.addEventListener('loadedmetadata', () => {
+      this.audio.volume = util.getVolume();
+      this.audio.currentTime = time;
+      this.setState({ isDisabled: false });
+    });
+
+    this.audio.addEventListener('durationchange', (event) => {
+      progress.max = Math.round(event.target.duration);
+    });
+
+    this.audio.addEventListener('timeupdate', (event) => {
+      progress.value = Math.round(event.target.currentTime);
+    });
   }
 
-  onVolumeChange = ({ target }) => {
-    util.setVolume(target.volume);
+  componentWillUnmount() {
+    this.audio.pause();
+    this.audio = null;
   }
 
-  onLoadedMetadata = () => {
-    this.audio.current.currentTime = util.getCurrentTime();
+  toggleSound = () => {
+    if (this.audio.paused) {
+      this.audio.play();
+    } else {
+      this.audio.pause();
+    }
+  };
+
+  changeTime = (event) => {
+    this.audio.currentTime = event.target.value;
   }
 
   render() {
-    const {
-      lang,
-      label,
-      url,
-      type,
-    } = this.props;
+    const { isDisabled } = this.state;
 
     return (
-      <div>
-        {(typeof window !== 'undefined') && (
-          <audio
-            ref={this.audio}
-            controls
-            preload="auto"
-            onVolumeChange={this.onVolumeChange}
-            onLoadedMetadata={this.onLoadedMetadata}
-            src={url}
-            type={type}
-            lang={lang}
-          >
-            <track
-              kind="captions"
-              label={label}
-              default
-            />
-          </audio>
-        )}
-      </div>
+      <fieldset
+        disabled={isDisabled}
+        className={css.player}
+      >
+        <button
+          type="button"
+          onClick={this.toggleSound}
+        >
+          play
+        </button>
+        <div>
+          <input
+            type="range"
+            ref={this.progress}
+            className={css.progress}
+            onInput={this.changeTime}
+            min="0"
+            step="1"
+          />
+        </div>
+      </fieldset>
     );
   }
 }
 
-export default Audio;
+export default AudioPlayer;
