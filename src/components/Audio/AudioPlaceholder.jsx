6a03e97a38648eb408f8aa7331e30fecbdd0f3d7
Alexander Zaytsev <zaytsev126@gmail.com>
2019-06-16 17:33:30 +0300

    Future rollback

--- src/components/Audio/AudioPlaceholder.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import css from './AudioPlayer.module.css';
-
-const AudioPlaceholder = () => (
-  <div className={css.player} />
-);
-
-export default AudioPlaceholder;
--- src/components/Audio/index.jsx
+++ src/components/Audio/index.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 
 import AudioPlayer from './AudioPlayer';
-import AudioPlaceholder from './AudioPlaceholder';
 
 export default props => (
   typeof window === 'undefined'
-    ? <AudioPlaceholder />
+    ? null
     : <AudioPlayer {...props} />
 );
