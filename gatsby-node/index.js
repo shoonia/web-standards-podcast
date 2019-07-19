9d345149e864b5dd4fc313a1ac1d567c76c57534
shoonia <zaytsev126@gmail.com>
2019-07-19 10:52:45 +0300

    Minify CSS class name

--- gatsby-node/index.js
+++ gatsby-node/index.js
@@ -1,4 +1,9 @@
 /* eslint-disable global-require */
+const ladash = require('lodash');
+const miniClassNames = require('mini-css-class-name/css-loader');
+
+const generate = miniClassNames();
+
 exports.createPages = async (gatsby) => {
   await require('./episode')(gatsby);
   await require('./podcast')(gatsby);
@@ -6,10 +11,25 @@ exports.createPages = async (gatsby) => {
   return Promise.resolve();
 };
 
-exports.onCreateWebpackConfig = ({ stage, actions }) => {
+exports.onCreateWebpackConfig = ({ stage, actions, getConfig }) => {
   if (stage === 'build-javascript') {
     actions.setWebpackConfig({
       devtool: false,
     });
   }
+
+  const config = getConfig();
+
+  config.module.rules = config.module.rules.map(item => ladash.cloneDeepWith(item, (value) => {
+    if (value && value.modules) {
+      return {
+        ...value,
+        localIdentName: undefined,
+        getLocalIdent: generate,
+      };
+    }
+    return undefined;
+  }));
+
+  actions.replaceWebpackConfig(config);
 };
